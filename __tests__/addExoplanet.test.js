271dc4ef55dea6e580193c4fe99cbf68ff2b9795
Oussama <oussama.elbouhtani@student.vinci.com>
2022-12-22 16:10:31 +0100

    feat : test should return false if the string contains a special character

--- __tests__/addExoplanet.test.js
+++ __tests__/addExoplanet.test.js
@@ -1,21 +1,16 @@
-const { checkUniqueName} = require("../models/Exoplanet");
+const { checkUniqueName } = require("../models/Exoplanet");
 
-
-
-  
-  describe('Test method checkUniqueName', () => {
-
-    
-      it('should return false if the string contains one lowerCase letter ', () => {
+describe("Test method checkUniqueName", () => {
+    it("should return false if the string contains one lowerCase letter ", () => {
         expect(checkUniqueName("TESt")).toEqual(false);
-    })
-    
-    it('should return true if the string contains no lowerCase letter ', () => {
-        expect(checkUniqueName("TEST")).toEqual(true);
-    })
-    
-    
-    
-
     });
-    
+
+    it("should return true if the string contains no lowerCase letter ", () => {
+        expect(checkUniqueName("TEST")).toEqual(true);
+    });
+
+    it("should return false if the string contains special character", () => {
+        expect(checkUniqueName("TEST*")).toEqual(false);
+    });
+
+});
